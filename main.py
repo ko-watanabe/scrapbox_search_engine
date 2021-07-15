9251b54880e50490f327290da3ce9222a92ed2ae
ko-watanabe <ko.watanabe.0522@gmail.com>
2021-07-15 09:28:21 +0200

    ADD - Crawl HTML DOM data from website

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.request import urlopen
 
 # Insert URL
 url = input('Enter URL: ')
@@ -6,15 +7,27 @@ url = input('Enter URL: ')
 # Request GET
 response = requests.get("https://scrapbox.io/api/pages/trackthink-search-engine/URL_storage_page/text")
 
-# 
+def crawlWebsiteData(url):
+    print("Crawl Data from website: " + url)
+    page = urlopen(url)
+    print(page)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    print(html)
+
+
+# Check if the page exists
 if (response.status_code == 200):
     body = response.text
     result = body.find(url)
 
+    # Check if the website is already stored
     if result >= 0:
-        print("SUCCESS")
+        print("END PROCESS : URL ALREADY STORED")
     else:
-        print("ERROR")
+        print("URL NOT EXISTS")
+        crawlWebsiteData(url)
 
 elif (response.status_code == 404):
-    print("Result not found!")
+    print("END PROCESS : PAGE NOT EXISTS")
+
