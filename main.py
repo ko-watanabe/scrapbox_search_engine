84b7e882877d4ceaf6c6daee0f300c16e87ae87f
ko-watanabe <ko.watanabe.0522@gmail.com>
2021-07-15 11:09:55 +0200

    ADD - chromedriver

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import chromedriver_binary
+from webdriver_manager.chrome import ChromeDriverManager
 
 def crawl_website_data(url):
     print("== BEGIN Crawling Data from website ==")
@@ -24,26 +28,36 @@ def crawl_website_data(url):
 
 def insert_data_into_scrapbox(url, title, body):
     print("== BEGIN Insert Title and Body in Scrapbox ==")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.google.com/')
+    sleep(5)
+    search_box = driver.find_element_by_name("q")
+    search_box.send_keys('ChromeDriver')
+    search_box.submit()
+    sleep(5)
+    driver.quit()
+
     print(url)
     print(title)
     print(body)
 
 # Prepare
-scrapbox_url = "https://scrapbox.io/api/pages/trackthink-search-engine/URL_storage_page/text" # TODO: Be able to choose Scrapbox url freely
-crawl_site_url = input('Enter URL: ')
+scrapbox_base_link = "https://scrapbox.io/api/pages/trackthink-search-engine" # TODO: Be able to choose Scrapbox url freely
+crawl_site_link = input('Enter URL: ')
 
 # RUN Main part
-response = requests.get(scrapbox_url)
+scrapbox_url_store_page_link = scrapbox_base_link + "/URL_storage_page/text"
+response = requests.get(scrapbox_url_store_page_link)
 
 if (response.status_code == 200):
     body = response.text
-    result = body.find(crawl_site_url)
+    result = body.find(crawl_site_link)
 
     # Check if the website is already stored
     if result >= 0:
         print("END PROCESS : URL ALREADY STORED")
     else:
-        title, body = crawl_website_data(crawl_site_url)
-        insert_data_into_scrapbox(crawl_site_url, title, body)
+        title, body = crawl_website_data(crawl_site_link)
+        insert_data_into_scrapbox(crawl_site_link, title, body)
 else:
     print("ERROR: Scrapbox URL store page not exists. Please create URL_storage_page")
